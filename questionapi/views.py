448d1854c4d0b7a5c294a4a7c87cc5e441c38ffe
Uday Upreti <uday@Udays-MacBook-Pro.local>
2022-07-14 22:37:26 -0700

    links fix

--- questionapi/views.py
+++ questionapi/views.py
@@ -56,12 +56,12 @@ class evaluation(APIView):
         if valid_data["question"]=="q1":
             labels = [['Monthly fee', 'Fee per user ', 'Charge', 'Cost', 'Average revenue per user', 'Monthly payment', 'Recurring monthly charge', 'Monthly service fee', 'Monthly access fee', 'Monthly maintenance fee', 'Membership fee', 'Subscription fee', 'Dues', 'charge per user', 'fee per user'], ['users', 'Subscribers', 'Customers', 'clients', 'patrons', 'payees', 'members', 'number of people using the service', 'number of users of the service', 'amount of people using the service', 'how many people are using the service', 'how popular is the service', 'how many users does the service have', 'is the service used by a lot of people', 'how well-known is the service', 'what is the user base for the service', 'how big is the market for the service'], ['Average customer lifetime', 'Customer lifespan', 'Duration of subscription', 'Churn', 'Attrition', 'employee turnover', 'Attrition rates', 'Average customer lifetime value', 'Average length of customer life cycle', 'Average time customers remain active', 'Customer attrition rate over time', 'How long do customers stay on average?', 'What is the typical lifespan of a customer?', 'staff turnover', 'Average client lifetime', 'Median customer lifetime', 'Ordinary customer lifetime']]
 
-            url = "http://ec2-3-110-221-230.ap-south-1.compute.amazonaws.com:5001/model/parse"
+            url = "http://ec2-65-1-1-17.ap-south-1.compute.amazonaws.com:5001/model/parse"
 
         else:
             labels = [['Make sure questions are easy to understand', 'Make sure questions are easy to direct', 'Make sure questions are easy to straightforward', 'Use short questions', 'Use succinct questions', 'Use clear questions', 'Avoid questions that are too long'], ['Keep questions open ended', 'Do not ask yes/no questions', 'Do not ask closed questions', 'Do not ask leading questions', 'Avoid questions that suggest the answer you want', "Don't ask directly what they want"], ['Have a logical structure to the questions', 'Questions should flow from one to the other', 'Questions should be sequenced in a natural way', 'Warm up before going into the detailed questions', 'Start with some easier questions to help them relax', 'Prepare your questions beforehand']]
 
-            url = "http://ec2-3-110-221-230.ap-south-1.compute.amazonaws.com:5002/model/parse"
+            url = "http://ec2-65-1-1-17.ap-south-1.compute.amazonaws.com:5002/model/parse"
 
         response = []
 
