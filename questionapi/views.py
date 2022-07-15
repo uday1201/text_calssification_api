6e0db05cdd86c50fda9d760360818edc75680b61
Uday Upreti <uday@Udays-MacBook-Pro.local>
2022-07-14 19:30:34 -0700

    url fix

--- questionapi/views.py
+++ questionapi/views.py
@@ -56,12 +56,12 @@ class evaluation(APIView):
         if valid_data["question"]=="q1":
             labels = [["Monthly fee", "Fee per user", "Charge", "Cost", "Average revenue per user", "Recurring monthly fee", "Fee charged per user", "Monthly service charge", "User fee", "Monthly subscription fee"], ["users", "Subscribers", "Customers", "clients", "patrons", "payees", "members"], ["Average lifetime", "lifespan", "Duration subscription", "Churn", "Attrition", "churn rate", "Churn analysis", "Churn prediction", "Churn prevention", "Reducing churn", "Increasing retention", "retention rate", "Customer attrition rate", "Attrition analysis", "Attrition prediction", "Attrition prevention"]]
 
-            url = "http://ec2-3-110-115-41.ap-south-1.compute.amazonaws.com:5001/model/parse"
+            url = "http://ec2-3-110-221-230.ap-south-1.compute.amazonaws.com:5001/model/parse"
 
         else:
             labels = [['Monthly fee', 'Fee per user ', 'Charge', 'Cost', 'Average revenue per user', 'Monthly payment', 'Recurring monthly charge', 'Monthly service fee', 'Monthly access fee', 'Monthly maintenance fee', 'Membership fee', 'Subscription fee', 'Dues', 'charge per user', 'fee per user'], ['users', 'Subscribers', 'Customers', 'clients', 'patrons', 'payees', 'members', 'number of people using the service', 'number of users of the service', 'amount of people using the service', 'how many people are using the service', 'how popular is the service', 'how many users does the service have', 'is the service used by a lot of people', 'how well-known is the service', 'what is the user base for the service', 'how big is the market for the service'], ['Average customer lifetime', 'Customer lifespan', 'Duration of subscription', 'Churn', 'Attrition', 'employee turnover', 'Attrition rates', 'Average customer lifetime value', 'Average length of customer life cycle', 'Average time customers remain active', 'Customer attrition rate over time', 'How long do customers stay on average?', 'What is the typical lifespan of a customer?', 'staff turnover', 'Average client lifetime', 'Median customer lifetime', 'Ordinary customer lifetime']]
 
-            url = "http://ec2-3-110-115-41.ap-south-1.compute.amazonaws.com:5002/model/parse"
+            url = "http://ec2-3-110-221-230.ap-south-1.compute.amazonaws.com:5002/model/parse"
 
         response = []
 
