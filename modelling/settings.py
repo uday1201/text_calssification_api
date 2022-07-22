32302f0055cf8e22e917fc27f53f7dfb4f313e4c
Uday Upreti <uday@Udays-MacBook-Pro.local>
2022-07-21 20:25:19 -0700

    fixes for streamlit #2

--- modelling/settings.py
+++ modelling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i^+l+=d-ov_-h-xjbez45rdweznid9hfs8vht3gy0e*hb9c1oy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
 
 
 # Application definition
